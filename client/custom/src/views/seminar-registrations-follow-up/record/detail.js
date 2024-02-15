5dcf3c49ec7e9cf680ae3e1e3211509dd1be7af7
abdulkadir <>
2024-02-15 13:57:55 +0300

    flex-direction row-reverse

--- client/custom/src/views/seminar-registrations-follow-up/record/detail.js
+++ client/custom/src/views/seminar-registrations-follow-up/record/detail.js
@@ -9,6 +9,20 @@ define('custom:views/seminar-registrations-follow-up/record/detail', ['views/rec
             this.setFieldReadOnly('contact');
         },
 
+        afterRender: function () {
+            Dep.prototype.afterRender.call(this);
+
+            const $recordGrid = this.$el.find('.record-grid');
+            const $left = $recordGrid.find('.left');
+            $recordGrid.css({
+                'display': 'flex',
+                'flex-direction': 'row-reverse',
+            });
+            $left.css({
+                'width': '300%',
+            });
+        },
+
         manageAccessEdit: function () {
             this.model.entityType = 'SeminarRegistrationsFollowUp';
 
