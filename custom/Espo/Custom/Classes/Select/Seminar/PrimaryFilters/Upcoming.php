b05e0d0558184183887b0a37fa386ebb4a577478
abdulkadir <>
2024-03-03 13:17:44 +0300

    upcoming filtre fix date

--- custom/Espo/Custom/Classes/Select/Seminar/PrimaryFilters/Upcoming.php
+++ custom/Espo/Custom/Classes/Select/Seminar/PrimaryFilters/Upcoming.php
@@ -2,15 +2,31 @@
 
 namespace Espo\Custom\Classes\Select\Seminar\PrimaryFilters;
 
+use DateTime;
+use Espo\Core\Select\Helpers\UserTimeZoneProvider;
 use Espo\Core\Select\Primary\Filter;
 use Espo\ORM\Query\SelectBuilder;
 
 class Upcoming implements Filter
 {
+    public function __construct(
+        private UserTimeZoneProvider $userTimeZoneProvider,
+    )
+    {
+    }
+
+    /**
+     * @throws \Exception
+     */
     public function apply(SelectBuilder $queryBuilder): void
     {
+
+        $timezone = $this->userTimeZoneProvider->get();
+        $now = new DateTime('now', new \DateTimeZone($timezone));
+
         $queryBuilder->where([
-            'seminarStartTime >=' => date('Y-m-d')
+//            'seminarStartTime >=' => $now->format('Y-m-d H:i:s')
+            'seminarStartTime >=' => date('Y-m-d H:i:s')
         ]);
     }
 }
